7093edf793d33889af7b2573de61e5df4ad44a0e
King <nigus.kidane@gmail.com>
2019-09-04 19:24:31 -0600

    Lombok Example Added.

---
+++ src/main/java/com/nk/lombokmapstructs/LombokMapstructsApplication.java
@@ -0,0 +1,13 @@
+package com.nk.lombokmapstructs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokMapstructsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokMapstructsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/nk/lombokmapstructs/model/Beer.java
@@ -0,0 +1,26 @@
+package com.nk.lombokmapstructs.model;
+
+import lombok.*;
+
+import java.util.UUID;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Beer {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+
+    public static void main(String[] args){
+        Beer beer=new Beer();
+        beer.id=UUID.randomUUID();
+        beer.beerName="Castle";
+        beer.beerStyle="Bottle";
+        beer.upc=100l;
+        System.out.println("The Name of The Beer Object is: "+beer);
+    }
+}
